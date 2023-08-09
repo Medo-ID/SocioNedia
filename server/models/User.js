d40ab0f3ac40f2f117ca1b219c20b40dbaf43f8d
MOHAMED IDAGHDOUR <midaghdour@gmail.com>
2023-08-09 22:19:34 +0100

    complete the back end section & setup the front end

---
+++ server/models/User.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            require: true,
+            min: 2,
+            max: 40
+        },
+        lastName: {
+            type: String,
+            required: true,
+            min: 2,
+            max: 40
+        },
+        email: {
+            type: String,
+            required: true,
+            max: 50,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true,
+            min: 8
+        },
+        picturePath: {
+            type: String,
+            default: ""
+        },
+        friends: {
+            type: Array,
+            default: []
+        },
+        location: String,
+        occupation: String,
+        viewedProfile: Number,
+        impressions: Number
+    }, { timestamps: true }
+)
+
+const User = mongoose.model("User", UserSchema)
+export default User
