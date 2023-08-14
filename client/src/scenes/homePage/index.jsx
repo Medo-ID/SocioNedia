5539ca939d954d59caccf6b383eb0749b31c0fe1
MOHAMED IDAGHDOUR <midaghdour@gmail.com>
2023-08-14 18:11:06 +0100

    userWidget && myPostWidget

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,11 +1,36 @@
-import { Box } from '@mui/material'
+import { Box, useMediaQuery } from '@mui/material'
 import React from 'react'
+import { useSelector } from 'react-redux'
 import Navbar from 'scenes/navBar'
+import MyPostWidget from 'scenes/widgets/MyPostWidget'
+import UserWidget from 'scenes/widgets/UserWidget'
 
 const HomePage = () => {
+  const isNonMobileScreen = useMediaQuery("(min-width:1000px)")
+  const { _id, picture } = useSelector((state) => state.user)
+  
   return (
     <Box>
       <Navbar />
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreen ? "flex" : "block"}
+        gap="0.5rem"
+        justifyContent="space-between"
+      >
+        <Box flexBasis={isNonMobileScreen ? "26%" : undefined}>
+          <UserWidget userId={_id} picturePath={picture} />
+        </Box>
+        
+        <Box
+          flexBasis={isNonMobileScreen ? "42%" : undefined}
+          mt={isNonMobileScreen ? undefined : "2rem"}
+        >
+          <MyPostWidget picturePath={picture} />
+        </Box>
+        {isNonMobileScreen && <Box flexBasis="26%"></Box>}
+      </Box>
     </Box>
   )
 }
--- client/src/theme.js
+++ client/src/theme.js
@@ -75,30 +75,30 @@ export const colorTokens = {
             }),
       },
       typography: {
-        fontFamily: ["Poppins", "Inter", "sans-serif"].join(","),
+        fontFamily: ["Poppins",  "sans-serif"].join(","),
         fontSize: 12,
         h1: {
-          fontFamily: ["Poppins", "Inter", "sans-serif"].join(","),
+          fontFamily: ["Poppins",  "sans-serif"].join(","),
           fontSize: 40,
         },
         h2: {
-          fontFamily: ["Poppins", "Inter", "sans-serif"].join(","),
+          fontFamily: ["Poppins",  "sans-serif"].join(","),
           fontSize: 32,
         },
         h3: {
-          fontFamily: ["Poppins", "Inter", "sans-serif"].join(","),
+          fontFamily: ["Poppins",  "sans-serif"].join(","),
           fontSize: 24,
         },
         h4: {
-          fontFamily: ["Poppins", "Inter", "sans-serif"].join(","),
+          fontFamily: ["Poppins",  "sans-serif"].join(","),
           fontSize: 20,
         },
         h5: {
-          fontFamily: ["Poppins", "Inter", "sans-serif"].join(","),
+          fontFamily: ["Poppins",  "sans-serif"].join(","),
           fontSize: 16,
         },
         h6: {
-          fontFamily: ["Poppins", "Inter", "sans-serif"].join(","),
+          fontFamily: ["Poppins",  "sans-serif"].join(","),
           fontSize: 14,
         },
       },
